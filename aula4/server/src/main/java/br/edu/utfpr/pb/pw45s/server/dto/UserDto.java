64e491220f7ed2b7638c1c94470f4692e705b992
Vinicius Pegorini <viniciuspegorini@gmail.com>
2025-09-08 16:19:26 -0300

    aula 4

---
+++ aula4/server/src/main/java/br/edu/utfpr/pb/pw45s/server/dto/UserDto.java
@@ -0,0 +1,27 @@
+package br.edu.utfpr.pb.pw45s.server.dto;
+
+import br.edu.utfpr.pb.pw45s.server.validation.UniqueUsername;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UserDto {
+
+    private long id;
+
+    @UniqueUsername
+    @NotNull
+    @Size(min = 4, max = 255)
+    private String username;
+
+    @NotNull
+    private String displayName;
+
+    @NotNull
+    @Size(min = 6, max = 254)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
+    private String password;
+
+}
